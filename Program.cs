6a323369a9732af7222345a2b13d4fe4835f0a50
William Bonner <williambonner@SydneyBonnersLaptop.local>
2020-09-03 10:54:57 -0500

    second commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace examplerepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! We want football.");
         }
     }
 }
